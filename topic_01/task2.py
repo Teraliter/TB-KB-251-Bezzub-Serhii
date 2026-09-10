2ff3f3ded5e157b29469d3d30f4424c00347c147
Serhiy <b.sergiy.an@stu.cn.ua>
2026-09-10 13:00:20 +0300

    add topic_01 tasks and report

---
+++ topic_01/task2.py
@@ -0,0 +1,8 @@
+text = "  hello world  "
+
+print(text.strip())
+print(text.capitalize())
+print(text.title())
+print(text.upper())
+print(text.lower())
+print(text.split())
---
+++ topic_01/task3.py
@@ -0,0 +1,5 @@
+def find_discriminant(a, b, c):
+    d = b**2 - 4 * a * c
+    return d
+
+print(find_discriminant(1, -3, 2))
